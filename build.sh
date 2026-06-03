97f07a4207b8aee92229a88e6726d933e1424978
engineerblack95 <engineerblack95@gmail.com>
2026-06-03 14:29:49 +0200

    Add Render build files and fix email backend

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput
+python manage.py ensure_admin 
